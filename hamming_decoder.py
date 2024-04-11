6c947663338e64fd3632a6db9c492fb360fca6e2
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-04-11 09:18:40 -0300

    add comments

--- hamming_decoder.py
+++ hamming_decoder.py
@@ -6,13 +6,22 @@ class HammingDecoder():
         self.__error_pattern_dict = HammingDecoder.error_pattern(self.__parity_matrix)
     
     def decode(self, message):
+        """
+        Decodes the message by summing (modulo 2) the message with the error associated with the syndrome.
+        The syndrome is obtained by the matricial product (modulo 2) of message (r = v + e) with the parity_matrix (H),
+        where v is the code-word and e is the error.
+        """
         matrix = np.remainder(message @ self.__parity_matrix, 2)
-        s = np.asarray(matrix).reshape(-1) #syndrome
-        e = self.__error_pattern_dict[str(s)]
+        s = np.asarray(matrix).reshape(-1) # syndrome
+        e = self.__error_pattern_dict[str(s)] # error
         return message ^ e
     
     @staticmethod
     def error_pattern(parity_matrix):
+        """
+        Determines the error pattern dict, which maps each syndrome (s) 
+        to its error (e) with the lowest Hamming weight.
+        """
         N = parity_matrix.shape[0]
         error_pattern_dict = {}
         error_pattern_dict[str(np.array([0, 0, 0]))] = np.zeros(N).astype(int)
--- hamming_encoder.py
+++ hamming_encoder.py
@@ -5,5 +5,9 @@ class HammingEncoder():
         self.__generating_matrix = generating_matrix
     
     def encode(self, information_bits):
+        """
+        Encodes the information_bits by doing the matricial product (modulo 2) 
+        with the generating_matrix (G).
+        """
         matrix = np.remainder(information_bits @ self.__generating_matrix, 2)
         return np.asarray(matrix).reshape(-1)
