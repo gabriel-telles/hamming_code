4f57017fbb8b3e9f37fb1a2f6c3bd85c6ca763a8
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-05-31 15:33:12 -0300

    completed belief_propagation algorithm (probably)

--- lab3/src/belief_propagation.py
+++ lab3/src/belief_propagation.py
@@ -22,8 +22,15 @@ class BeliefPropagation:
         self.ldpc_matrix = ldpc_matrix
         self.nodes_llr = np.zeros_like(ldpc_matrix)
         
-    
-    
+    def run(self, max_iter=10):
+        iterations_counter = 1
+        while True:            
+            self.transmit_v_nodes_messages()
+            if iterations_counter == max_iter or self.check_parity():
+                return self.decide_bits()
+            self.transmit_c_nodes_messages()
+            iterations_counter += 1
+        
     def transmit_v_nodes_messages(self):
         sum_cols = np.tile(self.nodes_llr.sum(axis=0), (self.nodes_llr.shape[0], 1))
         self.nodes_llr = (sum_cols - self.nodes_llr + self.channel_llr_matrix) * self.ldpc_matrix
@@ -41,8 +48,22 @@ class BeliefPropagation:
                     nodes_llr_aux[i][j] = min_module * sign_product
         self.nodes_llr = nodes_llr_aux
                 
-                        
+    def check_parity(self):
+        for i in range(self.nodes_llr.shape[0]):
+            row = self.nodes_llr[i]
+            index_mask = (self.ldpc_matrix[i] != 0)
+            row_except_zero = row[index_mask]
+            sign_product = np.prod(np.sign(row_except_zero))
+            if sign_product < 0:
+                return False    
+        return True
                 
+    def decide_bits(self):
+        sum_cols = self.nodes_llr.sum(axis=0)
+        Lf = sum_cols + self.channel_llr_matrix[0]
+        Lf[Lf > 0] = 0
+        Lf[Lf < 0] = 1 
+        return Lf # bits
                 
                 
             
--- lab3/src/gaussian_channel.py
+++ lab3/src/gaussian_channel.py
@@ -1,8 +1,16 @@
 import numpy as np
 
 class GaussianChannel:
-    def __init__ (self, sigma):
-        self.sigma = sigma
+    def __init__ (self, N0):
+        self.sigma = np.sqrt(N0/2)
     
-    def transmit_LLR(self, symbols):
-        return (2 / self.sigma ** 2) * (np.array(symbols) + np.random.normal(0, self.sigma, len(symbols)))
+    def transmit_LLR(self, bits):
+        symbols = GaussianChannel.bits_to_symbols(bits)
+        return (2 / self.sigma ** 2) * (symbols + np.random.normal(0, self.sigma, len(symbols)))
+    
+    @staticmethod
+    def bits_to_symbols(bits):
+        symbols = np.array(bits)
+        symbols[symbols == 1] = -1
+        symbols[symbols == 0] = 1
+        return symbols
