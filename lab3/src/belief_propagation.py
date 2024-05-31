bb400b5f34600d089c28f51c00845471896d0ef3
Samir Nunes da Silva <samir.silva12342@gmail.com>
2024-05-31 13:59:10 -0300

    c-node messages algorithm

--- lab3/src/belief_propagation.py
+++ lab3/src/belief_propagation.py
@@ -1,3 +1,4 @@
+from math import inf
 import numpy as np
 
 def generate_parity_matrix(dv, dc, N):
@@ -19,13 +20,32 @@ class BeliefPropagation:
     def __init__(self, channel_llr, ldpc_matrix):
         self.channel_llr_matrix = np.tile(channel_llr, (ldpc_matrix.shape[0], 1))
         self.ldpc_matrix = ldpc_matrix
-        self.v_nodes_llr = np.zeros_like(ldpc_matrix)
+        self.nodes_llr = np.zeros_like(ldpc_matrix)
+        
     
-    def update_v_nodes(self):
-        sum_cols = np.tile(self.v_nodes_llr.sum(axis=0), (self.v_nodes_llr.shape[0], 1))
-        self.v_nodes_llr = (sum_cols - self.v_nodes_llr + self.channel_llr_matrix) * self.ldpc_matrix
+    
+    def transmit_v_nodes_messages(self):
+        sum_cols = np.tile(self.nodes_llr.sum(axis=0), (self.nodes_llr.shape[0], 1))
+        self.nodes_llr = (sum_cols - self.nodes_llr + self.channel_llr_matrix) * self.ldpc_matrix
 
-    def update_c_nodes(self):
+    def transmit_c_nodes_messages(self):
+        nodes_llr_aux = np.zeros_like(self.nodes_llr)
+        for i in range(self.nodes_llr.shape[0]):
+            row = self.nodes_llr[i]
+            for j in range(len(row)):
+                if self.ldpc_matrix[i][j] != 0:
+                    row_except_j = np.delete(row, j)
+                    row_except_j_and_zero = row_except_j[row_except_j != 0]
+                    min_module = np.min(np.abs(row_except_j_and_zero))
+                    sign_product = np.prod(np.sign(row_except_j_and_zero))
+                    nodes_llr_aux[i][j] = min_module * sign_product
+        self.nodes_llr = nodes_llr_aux
+                
+                        
+                
+                
+                
+            
         
         
 
